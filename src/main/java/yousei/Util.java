cc39038389efb59fe91053e5e909d89dc2b20e1f
SoichiSumi <2013s10@edu.gifu-nct.ac.jp>
2016-05-23 15:06:15 +0900

    Utilのテストを追加

--- src/main/java/yousei/Util.java
+++ src/main/java/yousei/Util.java
@@ -7,7 +7,7 @@ import java.util.Map;
  * Created by s-sumi on 2016/05/23.
  */
 public class Util {
-    public Map<String, Integer> addMap(Map<String,Integer> map1, Map<String, Integer> map2){
+    public static Map<String, Integer> addMap(Map<String,Integer> map1, Map<String, Integer> map2){
         Map<String, Integer> res= new HashMap<>(map1);
         /*for(Map.Entry<String,Integer> e:map2.entrySet()){
             res.merge(e.getKey(), e.getValue(),
---
+++ src/test/java/yousei/UtilTest.java
@@ -0,0 +1,33 @@
+package yousei;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import static org.junit.Assert.*;
+
+/**
+ * Created by s-sumi on 2016/05/23.
+ */
+public class UtilTest {
+    @Test
+    public void addMapTest(){
+        Map<String,Integer> map1= new HashMap<>();
+        map1.put("a",1);
+        map1.put("c",5);
+        map1.put("d",11);
+
+        Map<String,Integer> map2= new HashMap<>();
+        map2.put("a",1);
+        map2.put("b",5);
+        map2.put("c",11);
+
+        Map<String,Integer> res=Util.addMap(map1,map2);
+        assertEquals(res.get("a"),Integer.valueOf(2));
+        assertEquals(res.get("b"),Integer.valueOf(5));
+        assertEquals(res.get("c"),Integer.valueOf(16));
+        assertEquals(res.get("d"),Integer.valueOf(11));
+        assertEquals(res.get("e"),null);
+    }
+
+}
