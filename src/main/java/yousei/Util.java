5a92e5e843d123db8e0444210f6b9040752099b2
SoichiSumi <2013s10@edu.gifu-nct.ac.jp>
2016-06-15 18:34:17 +0900

    デバッグ部分を削除

--- src/main/java/yousei/Util.java
+++ src/main/java/yousei/Util.java
@@ -205,7 +205,7 @@ public class Util {
      * @param vector
      * @return
      */
-    public static List<Integer> convertVector2List(Map<String, Integer> vector) throws Exception{
+    public static List<Integer> convertVector2List(Map<String, Integer> vector) {
         NodeClasses nc = new NodeClasses();
         List<Integer> list = new ArrayList<>(Collections.nCopies(nc.dictionary.size()+1, 0));
         for (Map.Entry<String, Integer> e : vector.entrySet()) {
@@ -216,7 +216,7 @@ public class Util {
         return list;
     }
 
-    public static File singleGenealogy2Arff(List<Map<String, Integer>> genealogy) throws Exception {
+    public static File singleGenealogy2Arff(List<Map<String, Integer>> genealogy) throws IOException {
         NodeClasses nc = new NodeClasses();
         File tmpFile = File.createTempFile("genealogy", ".arff", workingDir);
         BufferedWriter bw = new BufferedWriter(new FileWriter(tmpFile));
@@ -283,7 +283,7 @@ public class Util {
             newData.setClassIndex(predictNum);
         return newData;
     }
-    public static void enumNotFoundNodes(Set<String> set)throws Exception{
+    public static void enumNotFoundNodes(Set<String> set){
         System.out.println("\nNotFoundNodes:");
         NodeClasses nc=new NodeClasses();
         for (String s:set){
